6ba9c76981c7c86d95abe81da662e9af0e3fc4dd
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-09-25 00:23:40 -0700

    Use set to meet O(n) requirement

--- medium/128-longest_consecutive_sequence.js
+++ medium/128-longest_consecutive_sequence.js
@@ -2,31 +2,50 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function (nums) {
+var longestConsecutive = function(nums) {
+  let numSet = new Set(nums);
   let maxCount = 0;
-  let currCount = 1;
 
-  if(nums.length === 0) return maxCount;
+  for(const num of nums) {
+    if(!numSet.has(num - 1)) {
+      let currCount = 0;
 
-  const sortedNums = nums.sort((a,b) => a - b);
-
-  for(let i = 1; i < sortedNums.length; i++){
-      const currEle = sortedNums[i];
-      const prevEle = sortedNums[i - 1];
-
-      if(currEle === prevEle) continue;
-
-      if(currEle === prevEle + 1) {
-          currCount++;
-      } else {
-          maxCount = Math.max(maxCount, currCount);
-          currCount = 1;
+      while(numSet.has(num + currCount)) {
+        numSet.delete(num + currCount);
+        currCount++;
       }
+      maxCount = Math.max(currCount, maxCount)
+    }
   }
 
-  maxCount = Math.max(maxCount, currCount);
   return maxCount;
 };
+
+// var longestConsecutive = function (nums) {
+//   let maxCount = 0;
+//   let currCount = 1;
+
+//   if(nums.length === 0) return maxCount;
+
+//   const sortedNums = nums.sort((a,b) => a - b);
+
+//   for(let i = 1; i < sortedNums.length; i++){
+//       const currEle = sortedNums[i];
+//       const prevEle = sortedNums[i - 1];
+
+//       if(currEle === prevEle) continue;
+
+//       if(currEle === prevEle + 1) {
+//           currCount++;
+//       } else {
+//           maxCount = Math.max(maxCount, currCount);
+//           currCount = 1;
+//       }
+//   }
+
+//   maxCount = Math.max(maxCount, currCount);
+//   return maxCount;
+// };
 
 longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]);
 
